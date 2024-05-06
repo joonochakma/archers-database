56f5054cb4528d18328b63beb8fe4b543d7cdb6f
Joono Chakma <73777352+joonochakma@users.noreply.github.com>
2024-05-06 22:36:27 +1000

    Update: EquipmentDesc.sql

---
+++ EquipmentDesc.sql
@@ -0,0 +1,4 @@
+INSERT INTO EquipmentDescription(Equipment,Description) VALUES ('R','Recurve');
+INSERT INTO EquipmentDescription(Equipment,Description) VALUES ('C','Compound');
+INSERT INTO EquipmentDescription(Equipment,Description) VALUES ('B','Recurve/Compound Barebow');
+INSERT INTO EquipmentDescription(Equipment,Description) VALUES ('L','Longbow');
