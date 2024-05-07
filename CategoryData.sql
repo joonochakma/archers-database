c245e9c2af993e16585918f471e3c07953712610
Joono Chakma <73777352+joonochakma@users.noreply.github.com>
2024-05-08 04:37:44 +1000

    Update: CategoryData.sql

--- CategoryData.sql
+++ CategoryData.sql
@@ -1,6 +1,2 @@
-INSERT INTO Category (ArcherID,CompetitionID, RoundName, Classification, RegisterYear) VALUES ('10001','10006', 'WA70/1440', 'Female Open','2013');
-INSERT INTO Category (ArcherID,CompetitionID, RoundName, Classification,RegisterYear) VALUES ('10001','10003', 'AA50/1440', 'Female Open','2021');
-
-
-
-
+INSERT INTO Category (ArcherID,CompetitionID, RoundID, Classification, RegisterYear) VALUES ('10001','10006', '10001', 'Female Open','2013');
+INSERT INTO Category (ArcherID,CompetitionID, RoundID, Classification,RegisterYear) VALUES ('10001','10003', '10009', 'Female Open','2021');
