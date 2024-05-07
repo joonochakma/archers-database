d183c572592fb4584663b1f739e8eb151df0a9cf
Joono Chakma <73777352+joonochakma@users.noreply.github.com>
2024-05-07 23:01:03 +1000

    Update: ArcherDatabase.sql (more tables)

--- ArcherDatabase.sql
+++ ArcherDatabase.sql
@@ -1,17 +1,17 @@
 -- Drop table
 DROP TABLE IF EXISTS EndScores;
 DROP TABLE IF EXISTS Staging;
-DROP TABLE IF EXISTS Rounds;
 DROP TABLE IF EXISTS Category;
-DROP TABLE IF EXISTS ClubCompetition;
 DROP TABLE IF EXISTS DefaultEquipment;
+DROP TABLE IF EXISTS Rounds;
+DROP TABLE IF EXISTS ClubCompetition;
 DROP TABLE IF EXISTS EquipmentDescription;
 DROP TABLE IF EXISTS ArcherInfo;
 
 -- TABLES
 -- ArcherInfo_Table
 CREATE TABLE ArcherInfo (
-    ArcherID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     FirstName VARCHAR(255),
     LastName VARCHAR(255),
     Gender ENUM('M', 'F'),
@@ -20,17 +20,11 @@ CREATE TABLE ArcherInfo (
 
 -- Round_Table
 CREATE TABLE Rounds(
+    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     RoundName VARCHAR(255) NOT NULL,
-    Arrows90m CHAR(10) DEFAULT '0',
-    Arrows70m CHAR(10) DEFAULT '0',
-    Arrows60m CHAR(10) DEFAULT '0',
-    Arrows50m CHAR(10) DEFAULT '0',
-    Arrows40m CHAR(10) DEFAULT '0',
-    Arrows30m CHAR(10) DEFAULT '0',
-    Arrows20m CHAR(10) DEFAULT '0', 
-    Arrows10m CHAR(10) DEFAULT '0',
-    PRIMARY KEY (RoundName)
-);
+    TotalArrows CHAR(10) DEFAULT '0',
+    TotalPossibleScores CHAR(10) DEFAULT '0'
+) AUTO_INCREMENT=10000;
 
 
 
@@ -43,50 +37,60 @@ CREATE TABLE EquipmentDescription(
 
 -- Default Equipment
 CREATE TABLE DefaultEquipment(
-    RoundName VARCHAR(255) NOT NULL,
-    Category VARCHAR(255) NOT NULL ,
+    RoundID INT NOT NULL,
+    Category VARCHAR(255) NOT NULL,
     Equipment VARCHAR(5) NOT NULL,
-    FOREIGN KEY (RoundName) REFERENCES Rounds(RoundName),
+    FOREIGN KEY (RoundID) REFERENCES Rounds(ID),
     FOREIGN KEY (Equipment) REFERENCES EquipmentDescription(Equipment)
 );
 
 -- Club Competition
 CREATE TABLE ClubCompetition(
-    CompetitionID INT NOT NULL AUTO_INCREMENT,
+    ID INT NOT NULL AUTO_INCREMENT,
     CompetitionName VARCHAR(255),
     Championship VARCHAR(255) DEFAULT NULL,
-    PRIMARY KEY (CompetitionID)
+    PRIMARY KEY (ID)
 ) AUTO_INCREMENT = 10000;
 
 -- Category
 CREATE TABLE Category (
-    CategoryID INT NOT NULL AUTO_INCREMENT,
+    ID INT NOT NULL AUTO_INCREMENT,
     ArcherID INT NOT NULL,
     CompetitionID INT NOT NULL,
-    RoundName VARCHAR(255) NOT NULL,
+    RoundID INT NOT NULL,
     Classification  VARCHAR(255) NOT NULL,
     RegisterYear YEAR,
-    PRIMARY KEY (CategoryID),
-    FOREIGN KEY (ArcherID) REFERENCES ArcherInfo(ArcherID),
-    FOREIGN KEY (CompetitionID) REFERENCES ClubCompetition(CompetitionID),
-    FOREIGN KEY (RoundName) REFERENCES Rounds(RoundName)
+    PRIMARY KEY (ID),
+    FOREIGN KEY (ArcherID) REFERENCES ArcherInfo(ID),
+    FOREIGN KEY (CompetitionID) REFERENCES ClubCompetition(ID),
+    FOREIGN KEY (RoundID) REFERENCES Rounds(ID)
 ) AUTO_INCREMENT = 30000;
+
+CREATE TABLE Ranges(
+    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Distance ENUM('10m','20m','30m','40m','50m','60m','70m','90m')
+    ) AUTO_INCREMENT = 100000;
 
 -- ArrowScore_Table
 CREATE TABLE EndScores(
-    ScoreID INT NOT NULL AUTO_INCREMENT,
+    ID INT NOT NULL AUTO_INCREMENT,
     ArcherID INT NOT NULL,
     CategoryID INT NOT NULL,
-    EndNo ENUM('1','2','3','4','5','6','7','8','9','10','11','12'),
+    EndNo INT NOT NULL,
     ScoreDateTime DATETIME NOT NULL,  
-    Distance ENUM('20m', '30m', '40m', '50m', '60m', '70m', '90m'),
-    Arrow1 INT NOT NULL,
-    Arrow2 INT NOT NULL,
-    Arrow3 INT NOT NULL,
-    Arrow4 INT NOT NULL,
-    Arrow5 INT DEFAULT NULL,
-    Arrow6 INT DEFAULT NULL,
-    PRIMARY KEY (ScoreID),
-    FOREIGN KEY (CategoryID) REFERENCES Category(CategoryID),
-    FOREIGN KEY (ArcherID) REFERENCES ArcherInfo(ArcherID)
+    RangeID INT NOT NULL,
+    Score INT NOT NULL,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (CategoryID) REFERENCES Category(ID),
+    FOREIGN KEY (ArcherID) REFERENCES ArcherInfo(ID),
+    FOREIGN KEY (RangeID) REFERENCES Ranges(ID)
+    ) AUTO_INCREMENT = 100000;
+    
+
+    CREATE TABLE RoundArrows(
+    RoundID INT NOT NULL AUTO_INCREMENT,
+    RangeID INT NOT NULL,
+    ArrowsNo INT NOT NULL,
+    FOREIGN KEY (RoundID) REFERENCES Rounds(ID),
+    FOREIGN KEY (RangeID) REFERENCES Ranges(ID)
     ) AUTO_INCREMENT = 100000;
