af66da127108d30e42439fd1414fcbc2dddf0c10
Joono Chakma <73777352+joonochakma@users.noreply.github.com>
2024-05-08 05:08:12 +1000

    Create: lookupScore

---
+++ lookupScore.sql
@@ -0,0 +1,16 @@
+SELECT 
+    EndScores.ScoreDateTime AS ScoreDateTime,
+    Rounds.RoundName AS RoundName,
+    EndScores.Score AS Score
+FROM 
+    ArcherInfo
+INNER JOIN 
+    Category ON ArcherInfo.ID = Category.ArcherID
+INNER JOIN 
+    Rounds ON Rounds.ID = Category.RoundID
+INNER JOIN 
+    EndScores ON EndScores.CategoryID = Category.ID
+WHERE 
+    ArcherInfo.FirstName = 'Archer_First_Name'
+AND 
+    ArcherInfo.LastName = 'Archer_Last_Name';
