5b6127fb68f5c6a553c1183e95746dc534319499
Joono Chakma <73777352+joonochakma@users.noreply.github.com>
2024-05-15 05:17:08 +1000

    Update checkCategory.sql

--- queries/checkCategory.sql
+++ queries/checkCategory.sql
@@ -1,3 +1,13 @@
+/*
+This query retrieves detailed information about archers, including:
+- First name and last name.
+- Classification of the archer.
+- Default equipment for the archer's classification and round.
+- Registration year of the archer.
+- Name of the competition.
+*/
+
+    --
 SELECT
     ArcherInfo.FirstName,
     ArcherInfo.LastName,
