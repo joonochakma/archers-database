d166980b9e3898ec5e0b86bfc1e95c4c47e7f19d
Joono Chakma <73777352+joonochakma@users.noreply.github.com>
2024-05-16 20:39:16 +1000

    Update ArcherDatabase.sql
    New tables 
    - Division
    - Classification
    - StagingScores
    - Face

--- ArcherDatabase.sql
+++ ArcherDatabase.sql
@@ -16,7 +16,7 @@ CREATE TABLE ArcherInfo (
     LastName VARCHAR(255),
     Gender ENUM('M', 'F'),
     DOB DATE
-) AUTO_INCREMENT=10000;
+);
 
 -- Round_Table
 CREATE TABLE Rounds(
@@ -24,73 +24,110 @@ CREATE TABLE Rounds(
     RoundName VARCHAR(255) NOT NULL,
     TotalArrows CHAR(10) DEFAULT '0',
     TotalPossibleScores CHAR(10) DEFAULT '0'
-) AUTO_INCREMENT=10000;
+);
 
 
 
 -- Equipment description
-CREATE TABLE EquipmentDescription(
+CREATE TABLE Division(
+    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     Equipment VARCHAR(5) NOT NULL,
     Description VARCHAR(255) NOT NULL,
-    PRIMARY KEY (Equipment)
 );
 
 -- Default Equipment
 CREATE TABLE DefaultEquipment(
     RoundID INT NOT NULL,
     Category VARCHAR(255) NOT NULL,
-    Equipment VARCHAR(5) NOT NULL,
+    DivisionID INT NOT NULL.
     FOREIGN KEY (RoundID) REFERENCES Rounds(ID),
-    FOREIGN KEY (Equipment) REFERENCES EquipmentDescription(Equipment)
+    FOREIGN KEY (DivisionID) REFERENCES Division(ID)
 );
 
 -- Club Competition
 CREATE TABLE ClubCompetition(
-    ID INT NOT NULL AUTO_INCREMENT,
+    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     CompetitionName VARCHAR(255),
     Championship VARCHAR(255) DEFAULT NULL,
-    PRIMARY KEY (ID)
-) AUTO_INCREMENT = 10000;
+   
+);
 
 -- Category
 CREATE TABLE Category (
-    ID INT NOT NULL AUTO_INCREMENT,
+    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     ArcherID INT NOT NULL,
     CompetitionID INT NOT NULL,
     RoundID INT NOT NULL,
-    Classification  VARCHAR(255) NOT NULL,
+    ClassificationID  VARCHAR(255) NOT NULL,
     RegisterYear YEAR,
-    PRIMARY KEY (ID),
+    DivisionID INT NOT NULL,
+    
     FOREIGN KEY (ArcherID) REFERENCES ArcherInfo(ID),
     FOREIGN KEY (CompetitionID) REFERENCES ClubCompetition(ID),
-    FOREIGN KEY (RoundID) REFERENCES Rounds(ID)
-) AUTO_INCREMENT = 30000;
+    FOREIGN KEY (RoundID) REFERENCES Rounds(ID),
+    FOREIGN KEY (DivisionID) REFERENCES Division(ID),
+    FOREIGN KEY (ClassificationID) REFERENCES Classification(ID)
+);
 
 CREATE TABLE Ranges(
     ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     Distance ENUM('10m','20m','30m','40m','50m','60m','70m','90m')
-    ) AUTO_INCREMENT = 100000;
+    );
 
 -- ArrowScore_Table
 CREATE TABLE EndScores(
-    ID INT NOT NULL AUTO_INCREMENT,
+    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     ArcherID INT NOT NULL,
     CategoryID INT NOT NULL,
     EndNo INT NOT NULL,
     ScoreDateTime DATETIME NOT NULL,  
     RangeID INT NOT NULL,
     Score INT NOT NULL,
-    PRIMARY KEY (ID),
+
     FOREIGN KEY (CategoryID) REFERENCES Category(ID),
     FOREIGN KEY (ArcherID) REFERENCES ArcherInfo(ID),
     FOREIGN KEY (RangeID) REFERENCES Ranges(ID)
-    ) AUTO_INCREMENT = 100000;
+    );
     
 
     CREATE TABLE RoundArrows(
     RoundID INT NOT NULL AUTO_INCREMENT,
     RangeID INT NOT NULL,
     ArrowsNo INT NOT NULL,
+    FaceSymbol ENUM('+','*'),
+
     FOREIGN KEY (RoundID) REFERENCES Rounds(ID),
-    FOREIGN KEY (RangeID) REFERENCES Ranges(ID)
-    ) AUTO_INCREMENT = 100000;
+    FOREIGN KEY (RangeID) REFERENCES Ranges(ID),
+    FOREIGN KEY (FaceSymbol) REFERENCES Face(FaceSymbol)
+    );
+
+    CREATE TABLE Face(
+    FaceSymbol ENUM('+','*') PRIMARY KEY,
+    FaceSize ENUM('120cm','80cm'),
+    );
+
+    CREATE TABLE StagingScore(
+    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ArcherID INT NOT NULL,
+    CategoryID INT NOT NULL,
+    EndNo INT NOT NULL,
+    ScoreDateTime DATETIME NOT NULL,  
+    DivisionID INT NOT NULL,
+    RoundID INT NOT NULL,
+    CompetitionID INT NOT NULL,
+
+    FOREIGN KEY (CategoryID) REFERENCES Category(ID),
+    FOREIGN KEY (ArcherID) REFERENCES ArcherInfo(ID),
+    FOREIGN KEY (DivisionID) REFERENCES Division(ID),
+    FOREIGN KEY (RoundID) REFERENCES Rounds(ID),
+    FOREIGN KEY (CompetitionID) REFERENCES ClubCompetition(ID)
+    );
+
+    CREATE TABLE Classification(
+    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    MinimumAge INT NOT NULL,
+    MaaximumAge INT NOT NULL,
+    Gender ENUM('M', 'F'),
+    Description VARCHAR(255) NOT NULL,
+   
+    );
