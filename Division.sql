995c8994ec6b7114a36fce0783cc0a8020e656f2
Joono Chakma <73777352+joonochakma@users.noreply.github.com>
2024-05-18 22:10:15 +1000

    Update and rename EquipmentDesc.sql to Division.sql

---
+++ Division.sql
@@ -0,0 +1,4 @@
+INSERT INTO Division(Equipment,Description) VALUES ('R','Recurve');
+INSERT INTO Division(Equipment,Description) VALUES ('C','Compound');
+INSERT INTO Division(Equipment,Description) VALUES ('B','Recurve/Compound Barebow');
+INSERT INTO Division(Equipment,Description) VALUES ('L','Longbow');
